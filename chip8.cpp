976a045eaa44795e8a2b032019e041488d5b05ef
sjrichardson <samrichardson812@gmail.com>
2021-07-20 23:06:54 -0400

    Implement 8xy0 through 8xyE

--- chip8.cpp
+++ chip8.cpp
@@ -92,7 +92,7 @@ void chip8::OP_2nnn() {
 
 // Skip next instruction if Vx = kk
 void chip8::OP_3xkk() {
-    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vx =  (opcode & 0x0F00u) >> 8u;
     uint8_t byte = opcode & 0x00FFu;
 
     if (registers[Vx] == byte) {
@@ -129,9 +129,93 @@ void chip8::OP_6xkk() {
 }
 
 // Set Vx = Vx + kk
-void chip8::OP_7xkk {
+void chip8::OP_7xkk() {
     uint8_t Vx = (opcode & 0x0F00u) >> 8u;
     uint8_t byte = opcode & 0x00FFu;
 
     registers[Vx] += byte;
+}
+
+// Set Vx = Vy
+void chip8::OP_8xy0() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    registers[Vx] = registers[Vy];
+}
+
+// Set Vx = Vx OR Vy
+void chip8::OP_8xy1() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    registers[Vx] |= registers[Vy];
+}
+
+// Set Vx = Vx AND Vy
+void chip8::OP_8xy2() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    registers[Vx] &= registers[Vy];
+}
+
+// Set Vx = Vx XOR Vy
+void chip8::OP_8xy3() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    registers[Vx] ^= registers[Vy];
+}
+
+// Set Vx = Vx + Vy, set VF = carry
+void chip8::OP_8xy4() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    uint16_t sum = registers[Vx] + registers[Vy];
+    
+    // if sum > 255 set carry bit to 1
+    registers[0xF] = (sum > 0xFFu) 1 : 0;
+    // convert sum to uint8_t
+    registers[Vx] = sum & 0xFFu;
+}
+
+// Set Vx = Vx - Vy, set VF = NOT borrow
+void chip8::OP_8xy5() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+    
+    // if Vx > Vy set carry bit to 1
+    registers[0xF] = (registers[Vx] > registers[Vy]) ? 1 : 0;
+    registers[Vx] -= registers[Vy];
+}
+
+// Set Vx = Vx SHR 1
+void chip8::OP_8xy6() {
+    uint8_t Vx = (opcode & 0x0F00) >> 8u;
+
+    // Set LSB in VF
+    registers[0xF] = (registers[Vx] & 0x1u);
+    registers[Vx] >> 1;
+}
+
+// Set Vx = Vy - Vx, set VF = NOT borrow
+void chip8::OP_8xy7() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    // if Vy > Vx set carry bit to 1
+    registers[0xF] = (registers[Vy] > registers[Vx]) ? 1 : 0;
+    registers[Vx] = registers[Vy] - registers[Vx];
+}
+
+// Set Vx = Vx SHL 1
+void chip8::OP_8xyE() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+    // Save MSB in VF
+    //0x80 (128) is the most significant bit in a byte
+    registers[0xF] = (registers[Vx] & 0x80u) >> 7u;
+    registers[Vx] <<= 1;
 }
